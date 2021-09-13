eca6f141ec1ed9205ec42f68f59fe35d19f0adc0
xiaokai2hao <lkrexe@qq.com>
2021-09-13 16:11:16 +0800

    修改时间格式与nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,31 @@
+const moment = require('moment');
+
 module.exports = {
-    themeConfig: {
-      logo: 'assets/img/mi.jpg',
-      sidebar: [
-        '',
-        'about'
-      ],
-      lastUpdated: '更新时间',
-    }
+  plugins: [
+    [
+      '@vuepress/last-updated',
+      {
+        transformer: (timestamp) => {
+          // 不要忘了安装 moment
+          const moment = require('moment')
+          moment.locale('zh-cn')
+          return moment(timestamp).format('LLLL')
+        }
+      }
+    ]
+  ],
+  themeConfig: {
+    logo: 'assets/img/mi.jpg',
+    title: '小锴的个人简历',
+    sidebar: [
+      '',
+      'about'
+    ],
+    lastUpdated: '更新时间',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '个人简历', link: '/about/' },
+      // { text: 'External', link: 'https://google.com' },
+    ]
+  }
   }
